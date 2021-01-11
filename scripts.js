cb2183437fa9d55fb96f64535e0a72a496496a89
ValRobichaux <val.robichaux@gmail.com>
2021-01-10 19:23:12 -0600

    Created a side navigation bar and fixed about me picture

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("sideNavbar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("sideNavbar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
